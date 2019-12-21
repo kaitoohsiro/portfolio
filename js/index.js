8b8797d2f29584cbee8b22d080c354bd5a44d377
kaitooshiro <kaitooshiro@outlook.jp>
2019-12-22 00:46:19 +0900

    upload

---
+++ js/index.js
@@ -0,0 +1,42 @@
+var Vue = new Vue({
+    el: "#app",
+    data: {
+      website: true,
+      about: false,
+      hobby: false,
+      contact: false,
+    },
+    methods: {
+      link: function(one) {
+        if (one === "website") {
+          this.website = true;
+          this.about = false;
+          this.hobby = false;
+          this.contact = false;
+          this.oneX = false;
+        }
+        if (one === "about") {
+          this.website = false;
+          this.about = true;
+          this.hobby = false;
+          this.contact = false;
+          this.oneX = false;
+        }
+        if (one === "hobby") {
+          this.website = false;
+          this.about = false;
+          this.hobby = true;
+          this.contact = false;
+          this.oneX = false;
+        }
+        if (one === "contact") {
+          this.website = false;
+          this.about = false;
+          this.hobby = false;
+          this.contact = true;
+          this.oneX = false;
+        }
+      }
+    }
+  });
+  
